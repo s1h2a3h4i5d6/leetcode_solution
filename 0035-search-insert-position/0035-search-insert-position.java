d2933540ac34aee4e9c8922ca63fcb9dca18a050
Shahid Shekh <shahid.shekh24123072@gcoeara.ac.in>
2026-08-25 09:43:44 +0530

    Time: 0 ms (100%), Space: 44.4 MB (96.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,18 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=target){
-                return i;
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(target>nums[mid]){
+               start=mid+1;
+            }else if(target<nums[mid]){
+                end=mid-1;
+            }else{
+                return mid;
             }
         }
-        return nums.length;
-    }          
-}
+        return start;
+        }
+    }
