596423d2a6d53fa9bb708f34f801714316149e6b
Shahid Shekh <shahid.shekh24123072@gcoeara.ac.in>
2026-09-17 22:08:37 +0530

    Time: 1 ms (28.97%), Space: 47.3 MB (65.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,33 +1,56 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         // nums=[3,0,1]
+//         int i=0;
+//         while(i<nums.length){
+//             // lets consider correct ele for comparison
+//             int correct=nums[i];
+//             // now compare ele at correct and element at i if not same then swap
+//             // consider i<nums.length because it goes out of bound
+//             if(nums[i]<nums.length &&nums[i]!=nums[correct]){
+//                 int temp=nums[i];
+//                 nums[i]=nums[correct];
+//                 nums[correct]=temp;
+
+//             }
+//             // if if become false then it is sorted we have to move forward
+//             else{
+//                 i++;
+//             }
+//         }
+//             // now find missing one in sorted as nums are sorted then nums[i]==index
+//             for(i=0;i<nums.length;i++){
+//                 if(nums[i]!=i){
+//                     return i;
+//                 }
+//             }
+          
+   
+        
+//         // if not found it must be length of arr
+//         return nums.length;
+//     }
+// }
 class Solution {
-    public int missingNumber(int[] nums) {
-        // nums=[3,0,1]
+     public int missingNumber(int[] nums) {
         int i=0;
         while(i<nums.length){
-            // lets consider correct ele for comparison
             int correct=nums[i];
-            // now compare ele at correct and element at i if not same then swap
-            // consider i<nums.length because it goes out of bound
-            if(nums[i]<nums.length &&nums[i]!=nums[correct]){
+            if(nums[i]<nums.length&&nums[i]!=nums[correct]){
                 int temp=nums[i];
                 nums[i]=nums[correct];
                 nums[correct]=temp;
-
             }
-            // if if become false then it is sorted we have to move forward
             else{
                 i++;
             }
         }
-            // now find missing one in sorted as nums are sorted then nums[i]==index
-            for(i=0;i<nums.length;i++){
-                if(nums[i]!=i){
-                    return i;
-                }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=i){
+                return i;
             }
-          
-   
-        
-        // if not found it must be length of arr
+        }
         return nums.length;
+
     }
-}
+}    
